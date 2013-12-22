10db38f19ffebeb88c43f84c984f43ce6bd491d1
wanderwaltz <wanderwaltz@gmail.com>
2013-12-22 20:51:04 +0700

    Changed Ruby-style comments into Objective-C style comments.

--- RSpec/EmptyClass_10Lines.m
+++ RSpec/EmptyClass_10Lines.m
@@ -1,10 +1,10 @@
-#--------------------------------------------------------------------------
-#  EmptyClass_10Lines.m
-#  ObjectiveCritic
-#
-#  Created by Egor Chiglintsev on December 22, 2013.
-#  Copyright (c) 2013 Egor Chiglintsev. All rights reserved.
-#--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+//  EmptyClass_10Lines.m
+//  ObjectiveCritic
+//
+//  Created by Egor Chiglintsev on December 22, 2013.
+//  Copyright (c) 2013 Egor Chiglintsev. All rights reserved.
+//--------------------------------------------------------------------------
 
 @interface EmptyClass : NSObject
 @end
