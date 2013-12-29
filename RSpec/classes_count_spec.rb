0e5cfce35e89b69ec57a7c15e07944c19817aadc
wanderwaltz <wanderwaltz@gmail.com>
2013-12-29 15:54:35 +0700

    Updated grammar, added a real-world example (UIResponder.h)

--- RSpec/classes_count_spec.rb
+++ RSpec/classes_count_spec.rb
@@ -12,28 +12,43 @@ describe OCritic::FileStats::ClassesCount do
     @stat = OCritic::FileStats::ClassesCount.new
   end
 
+
+  def do_process_file(filename)
+    file = File.open('examples_classes_count/' + filename, 'r')
+    @stat.process_file(:info, file)
+  end
+
+
   describe '#process_file' do
     it 'should properly count class interfaces for cc_empty_single sample' do
-      process_file('cc_empty_single.h')
+      do_process_file('cc_empty_single.h')
       @stat.value.should be == 1
       @stat.class_names.should be == ['EmptyClass_Single']
     end
 
+
     it 'should properly count class interfaces for cc_empty_two sample' do
-      process_file('cc_empty_two.h')
+      do_process_file('cc_empty_two.h')
       @stat.value.should be == 2
       @stat.class_names.should be == ['EmptyClass_First', 'EmptyClass_Second']
     end
 
+
     it 'should properly count class interfaces for cc_empty_three sample' do
-      process_file('cc_empty_three.h')
+      do_process_file('cc_empty_three.h')
       @stat.value.should be == 3
       @stat.class_names.should be == ['EmptyClass_First','SecondEmptyClass','ThirdClass_AlsoEmpty']
     end
-  end
 
-  def process_file(filename)
-    file = File.open('examples_classes_count/' + filename, 'r')
-    @stat.process_file(:info, file)
+
+    it 'should properly count class interfaces for UIResponder sample' do
+      do_process_file('UIResponder.h')
+      @stat.value.should be == 5
+      @stat.class_names.should be == ['UIResponder',
+                                      'UIKeyCommand',
+                                      'UIResponder(UIResponderKeyCommands)',
+                                      'NSObject(UIResponderStandardEditActions)',
+                                      'UIResponder(UIResponderInputViewAdditions)']
+    end
   end
 end
